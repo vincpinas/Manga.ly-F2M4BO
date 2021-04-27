9e1b6cca7ba7a06e395fcb1d5381f2823de0467e
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-04-27 18:07:31 +0200

    :blue_heart: db local removed from .gitignore

---
+++ blob/MangaApi/db.local.php
@@ -0,0 +1,18 @@
+<?php
+    ini_set('display_errors',1);
+    // connection variables
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "manga.ly";
+
+    // mysqli connection
+    $con = new mysqli($host, $username, $password, $db);
+
+    // custom error reporting
+    if ($con->connect_error) {
+        print "<style>* {padding: 0; margin: 0;} body {height:100vh; font-family:sans-serif; display:flex; flex-direction:column; justify-content:center; align-items:center; text-align: center;}</style>";
+        print "<p style='font-size:2em;'>Failed to connect to MySQL: " . mysqli_connect_error() . "</p>";
+        exit;
+    }
+?>
