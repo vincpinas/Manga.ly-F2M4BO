5058f826ef0073000f3200d4322456da2293da0b
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-05-16 18:37:50 +0200

    :blue_heart: comments api posting action

--- blob/CommentApi/comments.php
+++ blob/CommentApi/comments.php
@@ -1,12 +1,12 @@
 <?php
 header("Content-Type:application/json");
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *');  
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 
-require '/../database.php';
+require __DIR__.'/../database.php';
 
 function msg($success,$status,$message,$type,$extra = []){
     return array_merge([
@@ -21,3 +21,36 @@ $db_connection = new Database();
 $conn = $db_connection->__dbConnection();
 
 $returnData = [];
+
+if($_SERVER["REQUEST_METHOD"] != "POST") {
+    $returnData = msg(0,404,'Page Not Found!','Error');
+} else {
+    $action = trim($_POST['action']);
+    $author = trim($_POST['author']);
+    $message = trim($_POST['message']);
+
+    if($action === 'post') {
+        $create_comment = "INSERT INTO `comments` ('id', 'author', 'message', 'time') 
+                           VALUES (:id, :author, :comment, :ctime)";
+        $insert_stmt = $conn->prepare($create_comment);
+
+        $insert_stmt->bindValue(':id', NULL, PDO::PARAM_STR);
+        $insert_stmt->bindValue(':author', $author, PDO::PARAM_STR);
+        $insert_stmt->bindValue(':comment', $message, PDO::PARAM_STR);
+        $insert_stmt->bindValue(':ctime', time(), PDO::PARAM_STR);
+
+        $insert_stmt->execute();
+    }
+
+
+    $comments = [];
+
+    $returnData = [
+        'success' => 1,
+        'message' => 'Successfully fetched all comments.',
+        'comments' => $comments,
+        'type' => 'Success'
+    ]
+};
+
+echo json_encode($returnData);
